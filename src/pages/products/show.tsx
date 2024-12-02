a7b7cc6e7e1f5fa94b692a949ebeec1c0d2d71fa
dvankevich <dvankevich@gmail.com>
2024-12-02 20:34:40 +0200

    add comment

--- src/pages/products/show.tsx
+++ src/pages/products/show.tsx
@@ -6,6 +6,9 @@ export const ShowProduct = () => {
   if (isLoading) {
     return <div>Loading...</div>;
   }
-
+// {data?.data.name}: Це вираз, який використовує опціональний ланцюг (?.). 
+// Він перевіряє, чи існує об'єкт data і його властивість data. Якщо обидва існують, то 
+// буде повернене значення name. Якщо ж data або data.data не існує 
+// (т.е. є null або undefined), то вираз не викличе помилку і поверне undefined.
   return <div>Product name: {data?.data.name}</div>;
 };
