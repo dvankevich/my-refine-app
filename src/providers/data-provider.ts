b0ac8164e4bbcadf9b5bd803a48b1dedb2207e68
dvankevich <dvankevich@gmail.com>
2024-12-02 21:01:56 +0200

    Updating a Record

--- src/providers/data-provider.ts
+++ src/providers/data-provider.ts
@@ -12,8 +12,20 @@ export const dataProvider: DataProvider = {
 
     return { data };
   },
-  update: () => {
-    throw new Error("Not implemented");
+  update: async ({ resource, id, variables }) => {
+    const response = await fetch(`${API_URL}/${resource}/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify(variables),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.status < 200 || response.status > 299) throw response;
+
+    const data = await response.json();
+
+    return { data };
   },
   getList: () => {
     throw new Error("Not implemented");
